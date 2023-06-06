e2a90560f3f88b833add1fb8b0ce4f6a9ba85782
Gabriel de Araujo Prediger <48864607+gabriextremo@users.noreply.github.com>
2023-06-06 10:18:52 -0300

    Update trabajo.R
    Update on the code for the analysis

--- trabajo.R
+++ trabajo.R
@@ -4,27 +4,42 @@ install.packages('tidyverse')
 library('dslabs')
 library('dplyr')
 library('tidyverse')
+options(digits=5)
 #Pegando o dataset
 Anime <- read.csv2("anime.csv", sep=",", dec=".")
 
 # Pegando apenas as informações relevantes para a analise
 mal <- Anime %>% select(score, scored_by, members, favorites, rating, sfw, start_season, demographics, studios, title_english)
 str(myanimelist)
-mal
+mal <- na.omit(mal)
 
 # "Limpando" as informações
 mal <- mal %>% mutate(sfw = case_when(sfw =='True'~ 1, sfw =='False'~ 0))
+mal <- mal %>% mutate(members = members/1000)
+mal <- mal %>% mutate(favorites = favorites/1000)
+mal <- mal %>% mutate(hype = mal$favorites/mal$members*100)
 
+mal$hype
+max(mal$hype)
+view(mal)
 # Alguns plots exploratorios
 hist(mal$score)
 
-fansDCarteirinha <- mal$favorites/mal$scored_by
+unique(mal$studios)
 
-unique(mal$demographics)
-
+#boxplots
 boxplot(mal$score ~ mal$demographics)
 boxplot(mal$score ~ mal$start_season)
 
+#ggplots
+ggplot(data=mal) + geom_point(mapping = aes(x=score, y=favorites, color=sfw))
+ggplot(data=mal) + geom_point(mapping = aes(x=hype, y=start_season))
 
-ggplot(data=mal) + geom_point(mapping=aes(x=members, y=start_season)) + facet_wrap(~ sfw, nrow=2)
-ggplot(data=mal) + geom_point(mapping = aes(x=score, y =demographics))
+ggplot(data=mal) + geom_point(mapping = aes(x = members, y = start_season)) + facet_wrap(~ sfw, nrow = 2)
+ggplot(data=mal) + geom_point(mapping = aes(x = score, y = demographics))
+ggplot(data=mal) + geom_point(mapping = aes(x = sfw, y = score))
+
+ggplot(data=mal) + geom_point(mapping = aes(x=members, y = favorites))
+
+#smoth
+ggplot(data=mal) + geom_smooth(mapping = aes(x=score, y=hype))
